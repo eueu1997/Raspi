38659389516938bb6dabfd0dd19f477d78d4f2e1
eueu1997 <42282444+eueu1997@users.noreply.github.com>
2021-12-03 09:35:09 +0100

    Delete lha.py

--- lha.py
+++
@@ -1,7 +0,0 @@
-import requests as request
-
-if __name__=="__main__":
-	r = request.get('http://localhost:8080/sensor1/temperature')
-	print(r.json())
-	r = request.get('http://localhost:8080/sensor1/humidity')
-	print(r.json())
